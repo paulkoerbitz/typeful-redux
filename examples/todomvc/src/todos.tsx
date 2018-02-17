e6b9419bfd94966a8757a60f25c8a8a51c7c4063
Paul Koerbitz <paul.koerbitz@gmail.com>
2018-02-17 17:51:25 +0100

    Todomvc example: support editing todos

--- examples/todomvc/src/todos.tsx
+++ examples/todomvc/src/todos.tsx
@@ -16,7 +16,14 @@ const TodoReducer = createReducer([] as TodoItem[])
         ...s.slice(0, idx),
         { task: s[idx].task, completed: !s[idx].completed },
         ...s.slice(idx + 1)
+    ])
+    ('edit', (s: TodoItem[], p: { idx: number; task: string; }) => [
+        ...s.slice(0, p.idx),
+        { ...s[p.idx], task: p.task },
+        ...s.slice(p.idx + 1)
     ]);
+
+type TodoDispatch = typeof TodoReducer.__dispatchType;
 
 type FilterType = 'all' | 'active' | 'completed';
 
@@ -35,18 +42,64 @@ const store = new StoreBuilder()
 interface TodoProps {
     item: TodoItem;
     toggle(): void;
+    edit(task: string): void;
 }
 
-const TodoComponent = (p: TodoProps) =>
-    <li onClick={p.toggle} className={p.item.completed ? "completed" : ""}>
-        <div className="view">
-            <input className="toggle" type="checkbox" checked={p.item.completed} />
-            <label>{p.item.task}</label>
-            <button className="destroy" />
-        </div>
-    </li>;
+interface TodoState {
+    editedTask: string | undefined;
+}
 
-type TodoDispatch = typeof TodoReducer.__dispatchType;
+const ENTER_KEY_CODE = 13;
+
+class TodoComponent extends React.Component<TodoProps, TodoState> {
+    constructor(p: TodoProps) {
+        super(p);
+        this.state = { editedTask: undefined };
+    }
+    private edit = () => {
+        this.setState({ editedTask: this.props.item.task });
+    }
+    private handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const editedTask = e.target.value;
+        this.setState({ editedTask });
+    }
+    private handleSubmit = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.keyCode === ENTER_KEY_CODE && this.state.editedTask != undefined) {
+            this.props.edit(this.state.editedTask.trim());
+            this.setState({ editedTask: undefined });
+        }
+    }
+    render() {
+        const { toggle, item } = this.props;
+        const liClass =
+            (item.completed ? "completed" : "") +
+            (this.state.editedTask != undefined ? " editing" : "");
+
+        return (
+            <li className={liClass}>
+                <div className="view">
+                    <input
+                        className="toggle"
+                        type="checkbox"
+                        checked={item.completed}
+                        onClick={toggle}
+                    />
+                    <label onDoubleClick={this.edit}>{this.props.item.task}</label>
+                    <button className="destroy" />
+                </div>
+                <input
+                    className="edit"
+                    type="text"
+                    value={this.state.editedTask}
+                    onChange={this.handleChange}
+                    onKeyDown={this.handleSubmit}
+                    onBlur={() => this.setState({ editedTask: undefined })}
+                />
+            </li>
+        );
+    }
+}
+
 
 type TodoListProps =
     & TodoDispatch
@@ -69,7 +122,7 @@ class TodoListComponent extends React.Component<TodoListProps> {
 
     render() {
         const {
-            todos, clear, toggle, clearCompleted,
+            todos, clear, toggle, edit, clearCompleted,
             filter, all, active, completed
         } = this.props;
 
@@ -82,7 +135,12 @@ class TodoListComponent extends React.Component<TodoListProps> {
         const itemName = visible.length === 1 ? "item" : "items";
 
         const items = visible.map((todo, idx) =>
-            <TodoComponent key={idx} item={todo} toggle={() => toggle(idx)} />
+            <TodoComponent
+                key={idx}
+                item={todo}
+                toggle={() => toggle(idx)}
+                edit={task => edit({ idx, task })}
+            />
         );
 
         return (
@@ -124,14 +182,12 @@ class TodoListComponent extends React.Component<TodoListProps> {
 }
 
 const mapStateToProps = (state: { todos: TodoItem[]; filter: FilterType; }) => ({
-    todos: state.todos,
-    filter: state.filter
+    ...state
 });
 
 const mapDisptachToProps = (dispatch: { todos: TodoDispatch; filter: FilterDispatch; }) => ({
     ...dispatch.todos, ...dispatch.filter
-
-})
+});
 
 const TodoListContainer =
     connect(mapStateToProps, mapDisptachToProps)(TodoListComponent);
