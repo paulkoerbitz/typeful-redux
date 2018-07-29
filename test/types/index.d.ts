288db16f14ff7635f7c2756bced86e4bb1ae340d
Paul Koerbitz <paul.koerbitz@gmail.com>
2018-07-30 00:05:19 +0200

    Delete dtslint based type tests

--- test/types/index.d.ts
+++
@@ -1,4 +0,0 @@
-// TypeScript Version: 2.8
-// import { ActionsFromHandlerMap } from 'typeful-redux';
-// export const EmptyHandlerMap = 'foo bar quox';
-// export type T0 = typeof EmptyHandlerMap extends object ? {} : undefined;
