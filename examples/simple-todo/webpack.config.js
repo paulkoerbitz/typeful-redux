85d32df3dd5c009002ff1dbda27b73c410727fa8
Paul Koerbitz <paul.koerbitz@gmail.com>
2018-08-24 23:44:45 +0200

    Get simple-todo to work again

--- examples/simple-todo/webpack.config.js
+++ examples/simple-todo/webpack.config.js
@@ -1,47 +1,40 @@
-const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = function () {
-    var plugins = [];
-    var externals = {};
-    var plugins = [new HtmlWebpackPlugin({
+module.exports = {
+    mode: 'development',
+    entry: './src/todos.tsx',
+    devtool: 'source-map',
+    plugins: [new HtmlWebpackPlugin({
         title: "Typeful Todos",
         template: "./src/index.html",
         hash: true,
         inject: true
-    })];
-    var output = {
+    })],
+    output: {
         path: path.join(__dirname, '/dist'),
         filename: "[name].js",
-    };
-    var resolve = {
+    },
+    resolve: {
         extensions: ['.js', '.ts', '.tsx'],
         modules: ['node_modules']
-    };
-    var modules = {
-        loaders: [{
+    },
+    module: {
+        rules: [{
             test: /\.tsx?$/,
             loader: 'ts-loader',
-            exclude: ['node_modules', 'test', 'script']
+            exclude: ['node_modules']
+        }, {
+            test: /\.css$/,
+            loader: ['style-loader', 'css-loader']
         }]
-    };
-    var devServer = {
+    },
+    devServer: {
         historyApiFallback: true,
-        contentBase: path.join(__dirname),
         disableHostCheck: true,
         watchContentBase: false,
         watchOptions: {
             watch: true
         }
-    };
-    return {
-        entry: './src/todos.tsx',
-        devtool: 'source-map',
-        output: output,
-        resolve: resolve,
-        module: modules,
-        externals: externals,
-        plugins: plugins
-    };
+    },
 };
